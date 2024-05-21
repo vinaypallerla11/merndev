80a979f0e61e2a2acd5b2131216bd249ba329c22
vinaypallerla11 <vinaypallerla96@gmail.com>
2024-05-21 14:55:53 +0530

    mern dev

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT 
 const MONGOURL = process.env.MONGO_URL;
 
 mongoose.connect(MONGOURL)
